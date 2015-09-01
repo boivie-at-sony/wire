0ff2ffec2b956791054926e993ca60ff2112a61f
Jake Wharton <jw@squareup.com>
2015-09-01 00:50:24 -0400

    Use FileSystem and Path for loading data.

--- wire-schema/src/main/java/com/squareup/wire/schema/Loader.java
+++ wire-schema/src/main/java/com/squareup/wire/schema/Loader.java
@@ -15,11 +15,13 @@
  */
 package com.squareup.wire.schema;
 
-import com.google.common.base.Joiner;
 import com.squareup.wire.schema.internal.parser.ProtoFileElement;
 import com.squareup.wire.schema.internal.parser.ProtoParser;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,86 +30,58 @@ import okio.Source;
 
 /** Load proto files and their transitive dependencies, parse them, and link them together. */
 public final class Loader {
-  private final IO io;
-  private final Map<String, ProtoFile> loaded = new LinkedHashMap<>();
+  private final List<Path> dirs;
+  private final Map<Path, ProtoFile> loaded = new LinkedHashMap<>();
 
-  public Loader(IO io) {
-    this.io = io;
+  public Loader(List<Path> dirs) {
+    this.dirs = dirs;
   }
 
-  /** Returns a loader that loads all files from {@code base}. */
-  public static Loader forSearchPaths(final List<String> paths) {
-    IO io = new IO() {
-      @Override public Location locate(String file) throws IOException {
-        for (String path : paths) {
-          File f = new File(path, file);
-          if (f.exists()) {
-            return Location.get(path, file);
-          }
-        }
-
-        throw new IOException("Could not locate " + file + " within any of the following: "
-                + Joiner.on(", ").join(paths) + ".");
-      }
-
-      @Override public Source open(Location location) throws IOException {
-        return Okio.source(new File(location.base(), location.path()));
-      }
-
-      @Override public String toString() {
-        return String.format("Loader(%s)", Joiner.on(":").join(paths));
-      }
-    };
-    return new Loader(io);
+  /** Returns a loader that loads all files from {@code paths}. */
+  public static Loader forSearchPaths(final List<Path> paths) {
+    return new Loader(paths);
   }
 
-  public Schema load(Iterable<String> sourceFiles) throws IOException {
-    for (String path : sourceFiles) {
+  public Schema load(Iterable<Path> sourceFiles) throws IOException {
+    for (Path path : sourceFiles) {
       load(path);
     }
     return new Linker(loaded.values()).link();
   }
 
-  private void load(String path) throws IOException {
+  private void load(Path path) throws IOException {
     if (loaded.containsKey(path)) {
       return;
     }
 
-    Location location;
-    try {
-      location = io.locate(path);
-    } catch (IOException e) {
-      throw new IOException("Failed to locate " + path + " with " + io, e);
+    ProtoFileElement element = null;
+    for (Path dir : dirs) {
+      if (path.isAbsolute() && !path.startsWith(dir)) {
+        continue;
+      }
+      Path resolvedPath = dir.resolve(path);
+      if (Files.exists(resolvedPath)) {
+        Location location = Location.get(dir.toString(), path.toString());
+        try (Source source = Okio.source(resolvedPath)) {
+          String data = Okio.buffer(source).readUtf8();
+          element = ProtoParser.parse(location, data);
+        } catch (IOException e) {
+          throw new IOException("Failed to load " + path, e);
+        }
+        break;
+      }
     }
-
-    ProtoFileElement element;
-    try (Source source = io.open(location)) {
-      String data = Okio.buffer(source).readUtf8();
-      element = ProtoParser.parse(location, data);
-    } catch (IOException e) {
-      throw new IOException("Failed to load " + location + " with " + io, e);
+    if (element == null) {
+      throw new FileNotFoundException("Failed to locate " + path + " in " + dirs);
     }
 
     ProtoFile protoFile = ProtoFile.get(element);
     loaded.put(path, protoFile);
 
     // Recursively load dependencies.
-    for (String importedFile : element.imports()) {
-      load(importedFile);
+    FileSystem fs = path.getFileSystem();
+    for (String importPath : element.imports()) {
+      load(fs.getPath(importPath));
     }
-  }
-
-  public interface IO {
-    /**
-     * Search for the location that holds the {@code .proto} file with path {@code path}. This may
-     * resolve using a single directory, a .jar file, or potentially a search path with both.
-     */
-    Location locate(String path) throws IOException;
-
-    /**
-     * Returns a source to read the {@code .proto} file at {@code location}. The location argument
-     * is always a return value from a previous call to {@code #locate}.
-     */
-    Source open(Location location) throws IOException;
   }
 }
